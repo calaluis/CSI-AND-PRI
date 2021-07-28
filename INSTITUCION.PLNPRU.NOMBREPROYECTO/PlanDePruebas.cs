990002472dffa0660248d08053c06866d5aad829
Luis Rodrigo Carrasco Lagos <calaluis@hotmail.com>
2021-07-27 20:24:23 -0400

    Se cambia ensamblado de pruebas, por uno que contempla un plan de pruebas de ejemplo.

---
+++ INSTITUCION.PLNPRU.NOMBREPROYECTO/PlanDePruebas.cs
@@ -0,0 +1,56 @@
+﻿using INSTITUCION.DAL.NOMBREPROYECTO.DBMS.OLEDB.Modulos.Capitales;
+using INSTITUCION.DAL.NOMBREPROYECTO.DBMS.OLEDB.Modulos.Personas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace INSTITUCION.PLNPRU.NOMBREPROYECTO
+{
+    [TestClass]
+    public class PlanDePruebas
+    {
+        [TestInitialize]
+        public void Arranque() 
+        {
+
+        }
+
+        [TestMethod("1.- Obtener lista de preguntas."), TestCategory("DAL"), Owner("Calaluis")]
+        public void ObtenerPreguntas()
+        {
+            CapitalesDal DAL = new CapitalesDal();
+            var ObtDatos = DAL.ObtenerPreguntas();
+            
+            if (!ObtDatos.decision) 
+            { Assert.Fail(ObtDatos.mensaje + " " + ObtDatos.mensajeDetalle); }
+            
+            if (!ObtDatos.estructuraDatos.Any()) 
+            { Assert.Inconclusive("Debiera de dar una lista de preguntas, para lo cual no retorna nada."); }
+        }
+
+        [TestMethod("2.- Obtener lista de sexo."), TestCategory("DAL"), Owner("Calaluis")]
+        public void ObtenerSexo() 
+        {
+            PersonasDal DAL = new PersonasDal();
+            var ObtDatos = DAL.ObtenerSexo();
+
+            if (!ObtDatos.decision) 
+            { Assert.Fail(ObtDatos.mensaje + " " + ObtDatos.mensajeDetalle); }
+
+            if (!ObtDatos.estructuraDatos.Any())
+            { Assert.Inconclusive("Debiera de dar una lista de sexo, para lo cual no retorna nada."); }
+        }
+
+        [TestMethod("3.- Obtener el universo de personas."), TestCategory("DAL"), Owner("Calaluis")]
+        public void ObtenerCantidadPersonas() 
+        {
+            PersonasDal DAL = new PersonasDal();
+            var ObtDatos = DAL.ObtenerCantidadPersonas();
+
+            if (!ObtDatos.decision)
+            { Assert.Fail(ObtDatos.mensaje + " " + ObtDatos.mensajeDetalle); }
+
+            if (ObtDatos.estructuraDatos < 1)
+            { Assert.Inconclusive("Debiera de dar una lista de sexo, para lo cual no retorna nada."); }
+        }
+    }
+}
--- INSTITUCION.PLNPRU.NOMBREPROYECTO/Properties/AssemblyInfo.cs
+++ INSTITUCION.PLNPRU.NOMBREPROYECTO/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("INSTITUCION.DAL.NOMBREPROYECTO.TEST")]
+[assembly: AssemblyTitle("INSTITUCION.PLNPRU.NOMBREPROYECTO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("INSTITUCION.DAL.NOMBREPROYECTO.TEST")]
+[assembly: AssemblyProduct("INSTITUCION.PLNPRU.NOMBREPROYECTO")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("8a2f2f1f-7889-422a-8cd1-3efacf992f46")]
+[assembly: Guid("0b3ab577-e461-44d9-a4a0-c1238f829e6e")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
